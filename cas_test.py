6475e8c7888671d8718985737c33cb51124ec679
RyushinF <104660792+RyushinF@users.noreply.github.com>
2022-05-10 19:20:58 +0900

    Update cas_test.py

--- cas_test.py
+++ cas_test.py
@@ -1,37 +1,28 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cascade_path = 'cascade.xml'
+img_out_path = '//home/rice/デスクトップ/apple_cascade/cascade_test/out/img'
+i = 0
+cap = cv2.VideoCapture(2)
 
 while True:
     ret, frame = cap.read()
     dst = frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
-    hsv_min = np.array([0,64,0])
-    hsv_max = np.array([30,255,255])
-    red_mask1 = cv2.inRange(hsv, hsv_min, hsv_max)
-
-    hsv_min = np.array([150,64,0])
-    hsv_max = np.array([179,255,255])
-    red_mask2 = cv2.inRange(hsv, hsv_min, hsv_max)
-
-    # 赤色領域のマスク（255：赤色、0：赤色以外）    
-    mask = red_mask1 + red_mask2
-
-    # マスキング処理
-    masked_img = cv2.bitwise_and(frame, frame, mask=mask)
-
-    gray = cv2.cvtColor(masked_img, cv2.COLOR_BGR2GRAY)
-
-    cascade = cv2.CascadeClassifier('cascade.xml')
+    cascade = cv2.CascadeClassifier(cascade_path)
     rect = cascade.detectMultiScale(gray, scaleFactor=1.07, minNeighbors=2, minSize=(1, 1))
-
+    #rect[lefttop_x, y, width, height]
     if len(rect) > 0:
         for rect in rect:
+            cv2.imwrite(str(img_out_path) + str(i) + '.png', frame[rect[1] : rect[1] + rect[3], rect[0] : rect[0] + rect[2]])
+            cv2.imshow('apple', frame[rect[1] : rect[1] + rect[3], rect[0] : rect[0] + rect[2]])
+            cv2.waitKey(1)
+            print(rect)
+            print("---------------------------------")
             cv2.rectangle(dst, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 0, 255), thickness=3)
-
+            i += 1
     cv2.imshow("out", dst)
     key = cv2.waitKey(1)
     if key == ord('q'):
